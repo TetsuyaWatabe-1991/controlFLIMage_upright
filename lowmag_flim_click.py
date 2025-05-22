e00a830e687c616a6b705e42e9212e42e4e3d22b
TetsuyaWatabe-1991 <yasudalab@mpfi.org>
2025-05-22 12:27:46 -0400

    minor changes

--- lowmag_flim_click.py
+++ lowmag_flim_click.py
@@ -4,14 +4,22 @@ import datetime
 from multi_z_stack_upright_functions import click_zstack_flimfile
 
 def click_flim_list(folder, filename_example):
+    print("click_flim_list")
+    print("filename_example: ", os.path.join(folder,filename_example))
     filelist = glob.glob(os.path.join(folder,filename_example))
+    if len(filelist) == 0:
+        print("No files found")
+        print("check the folder and filename_example")
+        return
     for each_flim in filelist:
         now = datetime.datetime.now()
         modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(each_flim))
         delta = (now - modified_date).total_seconds()
         if delta > 20:
             click_zstack_flimfile(each_flim, use_predefined_pos = True)
-            
+        else:
+            print("skipping: ", each_flim)
+            print("this file was modified less than 20 seconds ago")
     for each_flim in filelist:
         print('r"'+each_flim+'",')
 
